318070460eaf5f8f09a1bccd7b4220318627395e
Petr Kozelka <pkozelka@gmail.com>
2001-12-21 10:24:43 +0000

    fixed unit locations

--- demos/msxml/test/TestApp.dpr
+++ demos/msxml/test/TestApp.dpr
@@ -3,9 +3,6 @@ program TestApp(input, output);
 {$APPTYPE CONSOLE}
 
 uses
-	msxml_libxml in 'msxml_libxml.pas',
-	msxml_dom_impl in 'msxml_dom_impl.pas',
-	d5utils in 'd5utils.pas',
 	TestMain in 'TestMain.pas';
 
 {$R *.RES}
