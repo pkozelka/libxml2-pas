cec2c91b5f98b594fed165a1e0efdd4eda5f13c4
Martijn Brinkers <martijn_brinkers@yahoo.co.uk>
2002-01-16 16:48:13 +0000

    tests added

--- demos/dom2/DomTestSuite/tests/DomDocumentTests.pas
+++ demos/dom2/DomTestSuite/tests/DomDocumentTests.pas
@@ -31,11 +31,20 @@ type
       procedure checkNodeNameTest;
 
       (* from documentCreateAttribute.js *)
-      procedure createAttribute;
+      procedure createAttributeTest;
       (* from documentCreateAttributeMalformedXMLNS.js *)
       procedure createAttributeMalformedXMLNSTest;
       (* from documentCreateAttributeNullXMLNS.js *)
       procedure createAttributeNullXMLNSTest;
+      (* converted from: documentCreateAttributeXMLNSIllegalName.js *)
+      procedure createAttributeXMLNSIllegalNameTest;
+      (* converted from: documentCreateAttributeXMLNSPrefixXML.js *)
+      procedure createAttributeXMLNSPrefixXMLTest;
+      (* converted from: documentCreateCDATASection.js *)
+      procedure createCDATASectionTest;
+      (* converted from: documentCreateComment.js *)
+      procedure createCommentTest;
+
   end;
 
   function getUnitTests : ITestSuite;
@@ -76,15 +85,17 @@ begin
 end;
 
 (* creates an attribute and checks its properties *)
-procedure TDomDocumentFundamentalTests.createAttribute;
+procedure TDomDocumentFundamentalTests.createAttributeTest;
+const
+  ATTR_NAME = 'test';
 var
   document : IDomDocument;
   attr     : IDomAttr;
 begin
   document := fDomImplementation.createDocument('', '', nil);
-  attr := document.createAttribute('test');
+  attr := document.createAttribute(ATTR_NAME);
   checkNotNull(attr, 'attr = nil');
-  check(attr.nodeName = 'test', 'nodeName <> ''test''');
+  check(attr.nodeName = ATTR_NAME, 'nodeName is incorrect');
   check(attr.nodeName = attr.name, 'nodeName <> name');
   check(attr.value = '', 'value <> ''''');
   check(attr.nodeType = ATTRIBUTE_NODE, 'nodeType <> ATTRIBUTE_NODE');
@@ -139,6 +150,103 @@ begin
       check(e.code = NAMESPACE_ERR, 'NAMESPACE_ERR should have been thrown');
   end;
 end;
+
+
+
+(*
+ * checks if creating a attribute with a qualified name using illegal chars
+ * results in EDomException etInvalidCharacterErr
+*)
+procedure TDomDocumentFundamentalTests.createAttributeXMLNSIllegalNameTest;
+var
+  document      : IDomDocument;
+  attr          : IDomAttr;
+  qualifiedName : DomString;
+  prefix        : DomString;
+  namespaceURI  : DomString;
+  i             : Integer;
+begin
+  document     := fDomImplementation.createDocument('', '', nil);
+	namespaceURI := 'www.pingpolice.com';
+  prefix       := 'prefix:suffix';
+  for i := low(DomSetup.illegalChars) to high(DomSetup.illegalChars) do
+  begin
+    try
+      qualifiedName := prefix + DomSetup.illegalChars[i];
+      attr := document.createAttributeNS(namespaceURI, qualifiedName);
+
+      fail('EDomException INVALID_CHARACTER_ERR should have been thrown ' +
+           'but was not');
+    except
+      on e : EDomException do
+        check(e.code = INVALID_CHARACTER_ERR,
+                'INVALID_CHARACTER_ERR should be thrown but was: ' + e.message);
+    end;
+  end;
+end;
+
+
+
+(*
+ * creates an attribute with prefix xml and namespaceURI which is not uqual to
+ * http://www.w3.org/XML/1998/namespace. This should result in NAMESPACE_ERR
+*)
+procedure TDomDocumentFundamentalTests.createAttributeXMLNSPrefixXMLTest;
+var
+  document      : IDomDocument;
+  attr          : IDomAttr;
+  namespaceURI  : DomString;
+  qualifiedName : DomString;
+begin
+  document := fDomImplementation.createDocument('', '', nil);
+  namespaceURI  := 'http://www.w3.org/XML/1998/someothernamespaces';
+  qualifiedName := 'xml:local';
+  try
+    attr := document.createAttributeNS(namespaceURI, qualifiedName);
+    fail('EDomException NAMESPACE_ERR should have been thrown');
+  except
+    on e : EDomException do
+      check(e.code = NAMESPACE_ERR, 'NAMESPACE_ERR should have been thrown');
+  end;
+end;
+
+
+
+(* creates a CDataSection and checks its properties *)
+procedure TDomDocumentFundamentalTests.createCDATASectionTest;
+const
+  SECTION_NAME = 'new datasection';
+var
+  document     : IDomDocument;
+  cDataSection : IDomCDataSection;
+begin
+  document := fDomImplementation.createDocument('', '', nil);
+  cDataSection := document.createCDataSection(SECTION_NAME);
+  check(cDataSection.nodeName = '#cdata-section',
+          'CDATASection nodeName <> #cdata-section');
+  check(cDataSection.nodeValue = SECTION_NAME, 'nodevalue not correct');
+  check(cDataSection.nodeType = CDATA_SECTION_NODE);
+end;
+
+
+(* creates a comment and checks its properties *)
+procedure TDomDocumentFundamentalTests.createCommentTest;
+const
+  COMMENT_NAME = 'new comment';
+var
+  document : IDomDocument;
+  comment  : IDomComment;
+begin
+  document := fDomImplementation.createDocument('', '', nil);
+  comment := document.createComment(COMMENT_NAME);
+  check(comment.nodeName = '#comment',
+          'comment nodeName <> #comment');
+  check(comment.nodeValue = COMMENT_NAME, 'nodevalue not correct');
+  check(comment.nodeType = COMMENT_NODE);
+end;
+
+
+
 
 (******************************************************************************)
 (******************************************************************************)
