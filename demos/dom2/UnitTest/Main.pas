c46c7831351d8a5b59b7b86448c412201e782283
Uwe Fechner <ufechner@4commerce.de>
2002-01-31 22:16:36 +0000

    updated

--- demos/dom2/UnitTest/Main.pas
+++ demos/dom2/UnitTest/Main.pas
@@ -13,8 +13,16 @@ uses
   libxmldom,
   msxml_impl,
 
-  (* add new tests to uses class *)  
-  XPTest_xdom2_TestCase;
+  (* add new tests to uses class *)
+
+  XPTest_idom2_TestDOM2Methods,
+  XPTest_idom2_TestDomExceptions,
+  XPTest_idom2_TestMemoryLeaks,
+  XPTest_idom2_TestXPath,
+  XPTest_idom2_TestXSLT,
+  XPTest_idom2_TestPersist,
+  DomImplementationTests,
+  DomDocumentTests;
 
 (*
  * Returns a TestSuite made up of all individual test suits. This will be used
@@ -23,32 +31,37 @@ uses
  *
  * @param name the name of the All Test suite
 *)
-function getAllTests(const name : String) : ITestSuite;
+function getAllTests(const Name: string): ITestSuite;
 var
- allTestSuite : TTestSuite;
+  allTestSuite: TTestSuite;
 begin
- allTestSuite := TTestSuite.create(name);
+  allTestSuite := TTestSuite.Create(Name);
 
- (* add all test suits to the allTestSuite *)
- allTestSuite.addSuite(XPTest_xdom2_TestCase.getUnitTests);
+  (* add all test suits to the allTestSuite *)
 
- result := allTestSuite;
+  allTestSuite.addSuite(TTestDOM2Methods.Suite);
+  allTestSuite.addSuite(TTestDomExceptions.Suite);
+  allTestSuite.addSuite(TTestMemoryLeaks.Suite);
+  allTestSuite.addSuite(TTestXPath.Suite);
+  allTestSuite.addSuite(TTestXSLT.Suite);
+  allTestSuite.addSuite(TTestPersist.Suite);
+  allTestSuite.addSuite(TDomImplementationFundamentalTests.Suite);
+  allTestSuite.addSuite(TDomDocumentFundamentalTests.Suite);
+
+  Result := allTestSuite;
 end;
 
 initialization
   (*
-   * for every DomVendorID add a new line. This make sure that all tests will
+   * for every DomVendorID add a new line. This make sure that all tests
    * will be run for every Dom Implementation (specified by VendorID)
   *)
 
-  TestFramework.RegisterTest(
-      DomSetup.createDomSetupTest(MSXML2Rental, getAllTests('Ms-DOM Rental')));
+  TestFramework.RegisterTest(DomSetup.createDomSetupTest(MSXML2Rental,getAllTests('Ms-DOM Rental')));
 
-{remove comments to add testing of the Appartment Free Ms version }       
-//  TestFramework.RegisterTest(
-//      DomSetup.createDomSetupTest(MSXML2Free, getAllTests('Ms-DOM Free')));
+  {remove comments to add testing of the Appartment Free Ms version }
+  //  TestFramework.RegisterTest(
+  //      DomSetup.createDomSetupTest(MSXML2Free, getAllTests('Ms-DOM Free')));
 
-  TestFramework.RegisterTest(
-      DomSetup.createDomSetupTest(SLIBXML, getAllTests('Lib XML 2')));
-
+  TestFramework.RegisterTest(DomSetup.createDomSetupTest(SLIBXML,getAllTests('Lib XML 2')));
 end.
