42c478e321f632ac0ea60b173780087f70c84867
Petr Kozelka <pkozelka@gmail.com>
2002-02-10 16:15:52 +0000

    removed nonsenses: neither libxmldom nor comobj are part of this project

--- demos/dom2/test/Dom2Demo.dpr
+++ demos/dom2/test/Dom2Demo.dpr
@@ -3,9 +3,7 @@ program Dom2Demo;
 uses
   Forms,
   Unit1 in 'Unit1.pas' {Form1},
-  jkDomTest in 'jkDomTest.pas',
-  libxmldom in '..\..\..\Open_xdom\libxmldom.pas',
-  ComObj in 'D:\Programme\Borland\Delphi6\Source\Rtl\Common\ComObj.pas';
+  jkDomTest in 'jkDomTest.pas';
 
 {$R *.res}
 
@@ -14,3 +12,4 @@ begin
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
+
