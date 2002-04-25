9f4048cff5b6f19146d422cb8fc08aab3a343e55
Petr Kozelka <pkozelka@gmail.com>
2002-04-25 18:27:54 +0000

    xslt.h converted as a separate file

--- libxslt/libxslt.pas
+++ libxslt/libxslt.pas
@@ -1,54 +1,31 @@
-{
-    ------------------------------------------------------------------------------
-    This unit collects all the translated headers of libxslt (aka gnome-xslt).
-    Copyright for the header translation:
-        4commerce technologies AG
-        Kamerbalken 10-14
-        22525 Hamburg, Germany
-    Published under a double license:
-    a) the GNU Library General Public License:
-       http://www.gnu.org/copyleft/lgpl.html
-    b) the Mozilla Public License:
-       http://www.mozilla.org/MPL/MPL-1.1.html
-    Please send corrections to: ufechner@4commerce.de
-    See also:
-        http://xmlsoft.org/XSLT/  - the libxslt homepage
-    ------------------------------------------------------------------------------
-}
 unit libxslt;
+{
+  ------------------------------------------------------------------------------
+  Translated into pascal with help of h2pas utility from the FreePascal project.
+  Uwe Fechner <ufechner@4commerce.de> - 4commerce technologies AG
+  Petr Kozelka <pkozelka@email.cz>
+  ------------------------------------------------------------------------------
+}
 
 interface
 
 uses libxml2;
 
 const
-{$ifdef WIN32}
+{$IFDEF WIN32}
   LIBXSLT_SO = 'libxslt.dll';
-{$endif}
-{$ifdef LINUX}
+{$ENDIF}
+{$IFDEF LINUX}
   LIBXSLT_SO = 'libxslt.so';
-{$endif}
+{$ENDIF}
 
 {$WEAKPACKAGEUNIT}
 
 {$MINENUMSIZE 4}
 {$ASSERTIONS OFF}
 
-const
-  XSLT_DEFAULT_VERSION = '1.0';
-  XSLT_DEFAULT_VENDOR = 'libxslt';
-  XSLT_DEFAULT_URL = 'http://xmlsoft.org/XSLT/';
-
-  XSLT_NAMESPACE = 'http://www.w3.org/1999/XSL/Transform';
-
-{
-  Global cleanup function
-}
-procedure xsltCleanupGlobals; cdecl; external LIBXSLT_SO;
-{
-NOTE: two spaces after $I mean that the file is not yet converted to CVS-SIGN format.
-}
-
+{$I libxslt_xslt.inc}
+{$I libxslt_xsltwin32config.inc}
 {$I libxslt_numbersInternals.inc}
 {$I libxslt_xsltInternals.inc}
 {$I libxslt_transform.inc}
@@ -67,10 +44,7 @@ NOTE: two spaces after $I mean that the file is not yet converted to CVS-SIGN fo
 {$I libxslt_imports.inc}
 {$I libxslt_variables.inc}
 
-{$I libxslt_xsltwin32config.inc}
-
 {TODO: $I  libxslt_xsltconfig.inc}
-{TODO: $I  libxslt_xslt.inc}
 
 {TODO: libexslt !!!}
 
