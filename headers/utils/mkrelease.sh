8a4c2f171eb78ac0ccf950261ab203ff203931a0
Petr Kozelka <pkozelka@gmail.com>
2002-06-28 22:27:42 +0000

    MAJOR UPDATE: - target platform detection - current platform detection - generated binary package contains compiled units for all PAS files

--- headers/utils/mkrelease.sh
+++ headers/utils/mkrelease.sh
@@ -27,18 +27,30 @@ function checkSrcVersions()
 	grepWarn "LIBEXSLT_DOTTED_VERSION = '"$LIBEXSLT_VERSION"'" src/libexslt_exsltconfig.inc 'LIBEXSLT_DOTTED_VERSION[[:space:]]*='
 }
 
-function copySourceFilesToDist()
+function copyCommonSourcesToDist()
 {
+	rm -rf $DIST
+	mkdir $DIST
+
 	# copy files to be released there
 	mkdir $DIST/src
 	cp $SRC/*.inc $SRC/*.pas $DIST/src
 	cp $HEADERS_DIR/*.txt $DIST
 	cp $HEADERS_DIR/../common/license/COPYING* $DIST
 
-	if $CYGWIN; then
-		cp $SRC/libxml2_pas.dpk $DIST/src
-		# prepare resource script file
-		DCC_VER=`$DCC --version | tr -d '\015' | tr '\012' ';'` 
+	# prepare JEDI info file
+sed -f - $HEADERS_DIR/INFO.txt.template > $DIST/INFO.txt <<EOF
+s:@DATE@:`date +'%d %b %Y'`:g
+s:@LIBXML_VERSION@:$LIBXML_VER:g
+s:@LIBXSLT_VERSION@:$LIBXSLT_VER:g
+s:@LIBEXSLT_VERSION@:$LIBEXSLT_VER:g
+EOF
+}
+
+function prepareWin32Src()
+{
+	# prepare resource script file
+	DCC_VER=`$DCC --version | tr -d '\015' | tr '\012' ';'` 
 sed -f - $SRC/libxml2_pas.rc.template > $DIST/src/libxml2_pas.rc <<EOF
 s:@COMPILER_VERSION@:$DCC_VER:g
 s:@CURRENT_YEAR@:`date +%Y`:g
@@ -47,6 +59,8 @@ s:@LIBXML_MINOR_VERSION@:$LIBXML_MINOR_VERSION:g
 s:@LIBXML_MICRO_VERSION@:$LIBXML_MICRO_VERSION:g
 EOF
 
+	cp $SRC/libxml2_pas.dpk $DIST/src
+	if $CYGWIN; then
 		# create the resource file
 		pwd=`pwd`
 		cd $DIST/src
@@ -54,26 +68,45 @@ EOF
 		rm libxml2_pas.rc
 		cd $pwd
 	else
-		# remove resource include directive (I don't know how to make resources in Kylix)
-		sed 's/\$R \*.res//' $SRC/libxml2_pas.dpk >$DIST/src/libxml2_pas.dpk
-	fi
-	# prepare JEDI info file
-sed -f - INFO.txt.template > $DIST/INFO.txt <<EOF
-s:@DATE@:`date +'%d %b %Y'`:g
-s:@LIBXML_VERSION@:$LIBXML_VER:g
-s:@LIBXSLT_VERSION@:$LIBXSLT_VER:g
-s:@LIBEXSLT_VERSION@:$LIBEXSLT_VER:g
+
+cat - >$DIST/src/HOWTO-CreateRes.txt <<EOF
+Before you compile the package, you need to supply the libxml2_pas.res file
+that is bound to the resulting .BPL file.
+It can be created with this command:
+	brcc32.exe libxml2_pas.rc
+The file could not included, because the distribution was created on Linux.
 EOF
+
+	fi
+}
+
+function prepareLinuxSrc()
+{
+	# remove resource include directive (I don't know how to compile resources under Linux)
+	sed 's/\$R \*.res//' $SRC/libxml2_pas.dpk >$DIST/src/libxml2_pas.dpk
 }
 
 function makeZip()
 {
-	zip -qr $HEADERS_DIR/libxml2-pas-$LIBXML_VER-src.zip *
+	prepareWin32Src
+	pwd=`pwd`
+	cd $DIST
+	local archFile=$HEADERS_DIR/libxml2-pas-$LIBXML_VER-src.zip
+	zip -qr $archFile.part *
+	mv $archFile.part $archFile
+	cd $pwd
 }
+
 
 function makeTgz()
 {
-	tar -zcf $HEADERS_DIR/libxml2-pas-$LIBXML_VER-src.tgz *
+	prepareLinuxSrc
+	pwd=`pwd`
+	cd $DIST
+	local archFile=$HEADERS_DIR/libxml2-pas-$LIBXML_VER-src.tgz
+	tar -zcf $archFile.part *
+	mv $archFile.part $archFile
+	cd $pwd
 }
 
 function convertToLf()
@@ -107,13 +140,24 @@ function convertToCrLf()
 
 function compressDist()
 {
-	cd $DIST
 	if [ "$TERM" == "cygwin" ]; then
+		# native part
+		copyCommonSourcesToDist
 		makeZip
+
+		# foreign part
+		copyCommonSourcesToDist
+		cd $DIST
 		convertToLf `find . -type f`
 		makeTgz
 	else
+		# native part
+		copyCommonSourcesToDist
 		makeTgz
+
+		# foreign part
+		copyCommonSourcesToDist
+		cd $DIST
 		convertToCrLf `find . -type f`
 		makeZip
 	fi
@@ -121,9 +165,25 @@ function compressDist()
 
 function compile()
 {
+	if $CYGWIN; then
+		prepareWin32Src
+		pack='zip -qr '
+	else
+		prepareLinuxSrc
+		pack='tar -zcf '
+	fi
+	pwd=`pwd`
 	cd $DIST/src
-	$DCC -H -Q -N. -E. libxml2_pas.dpk
-	zip -qr $HEADERS_DIR/libxml2-pas-$LIBXML_VER-D6.zip *.dcp *.bpl
+	for fn in *.pas libxml2_pas.dpk; do
+		echo "Compiling $fn:"
+		$DCC -H -Q -N. -E. $fn
+	done
+	rm libxml2_pas.dcu
+	mkdir -p $DIST/lib
+	cp *.dcp *.bpl *.dcu $DIST/lib
+	cd $DIST
+	$pack $HEADERS_DIR/libxml2-pas-$LIBXML_VER-$DV.zip lib
+	cd $pwd
 }
 
 # check that LIBXML2_PAS env. var. is correct
@@ -132,14 +192,42 @@ if [ ! -d "$LIBXML2_PAS" ]; then
 	exit -1
 fi
 
-CYGWIN="false"
-DCC="dcc"
+DELPHIVERSION=`dcc --version | head -2 | tail -1 | sed 's/[^[:digit:]]*//'`
 case `uname` in
 CYGWIN*)
 	CYGWIN="true"
 	DCC="dcc32"
+	case "$DELPHIVERSION" in
+	13*)
+		DV='D5'
+		;;
+	14*)
+		DV='D6'
+		;;
+	*)
+		echo "Unknown Delphi version: $DELPHIVERSION"
+		exit -1
+		;;
+	esac
+	;;
+*)
+	CYGWIN="false"
+	DCC="dcc"
+	case "$DELPHIVERSION" in
+	13*)
+		DV='K1'
+		;;
+	14*)
+		DV='K2'
+		;;
+	*)
+		echo "Unknown Kylix version: $DELPHIVERSION"
+		exit -1
+		;;
+	esac
 	;;
 esac
+
 # check that GNOMECVSROOT env. var. is correct
 if [ ! -d "$GNOMECVSROOT/gnome-xml" ]; then
 	echo "ERROR - variable GNOMECVSROOT must point to the root of libxml2-pas working copy" >/dev/stderr
@@ -168,23 +256,26 @@ TMP=${TMP:-"$HOME/tmp"}/libxml2-pas-$LIBXML_VER
 rm -rf $TMP
 mkdir -p $TMP
 DIST=$TMP/dist
-rm -rf $DIST
-mkdir $DIST
-
 aCmd=$1
 
-case $aCmd in
+case "$aCmd" in
 dist)
 	checkSrcVersions
-	copySourceFilesToDist
 	compressDist
 	;;
 upload)
+#ftp-put.sh upload.log upload.sourceforge.net incoming $1 $1 anonymous libxml2-pas@sourceforge.net
+#cat upload.log
+#rm -f upload.log
 	echo "NOT IMPLEMENTED YET"
 	;;
 bin)
 	checkSrcVersions
-	copySourceFilesToDist
+	copyCommonSourcesToDist
 	compile
+	;;
+*)
+	echo "Invalid command: $aCmd" >/dev/stderr
+	exit -1
 	;;
 esac
