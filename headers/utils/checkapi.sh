d40c6bbf80b1c89cddbd08816f09571faf4bbfd0
Petr Kozelka <pkozelka@gmail.com>
2002-10-11 18:57:24 +0000

    check for untranslated header files

--- headers/utils/checkapi.sh
+++ headers/utils/checkapi.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # Usage:
@@ -9,6 +9,25 @@
 #   - check that LIBXML2_PAS variable is correctly set
 #   - check that <localroot> is a valid CVS working copy
 #   - cache files in localroot for later use in diff
+
+# translate file into pascal and apply known conversions
+function toPascal()
+{
+	local aFileName=$1
+	if [ "x$H2PAS" != "x" ] ; then
+		echo "---------------- [$aFileName] -----------------" >>$TMP/h2pas.log
+		# translate
+		echo "  translating $aFileName to $aFileName.1.pas"
+		h2pas -d -c -i $aFileName -o $aFileName.1.pas 1>&2 >>$TMP/h2pas.log
+
+		# apply known replacements
+		echo "  applying additional conversions --> $aFileName.2.pas"
+		sed -f "$LIBXML2_PAS/headers/utils/afterconv.sed" $aFileName.1.pas > $aFileName.2.pas
+
+		# compare original translation and the one with replacements
+		diff $aFileName.1.pas $aFileName.2.pas >$aFileName.pas.diff12
+	fi
+}
 
 LOCALROOT=$1
 shift 1
@@ -24,6 +43,7 @@ fi
 
 
 rm -rf $TMP $RESULTFILE
+mkdir $TMP
 
 THISPATH=`pwd`
 for fn in $FILELIST ; do
@@ -40,6 +60,10 @@ for fn in $FILELIST ; do
 	ORIGFILEPATH=`dirname $ORIGFILE`
 	REV=`echo $LINE | cut -d: -f3`
 	CVSENTRY=`grep "^/$ORIGFILENAME/" $LOCALROOT/$ORIGFILEPATH/CVS/Entries`
+
+	echo "$ORIGFILEPATH/$ORIGFILENAME" >>$TMP/allFiles
+	echo "$ORIGFILEPATH" >>$TMP/allPaths1
+
 	if [ "x$CVSENTRY" = "x" ]; then
 		echo
 		echo "ERROR: file $ORIGFILENAME not found in $LOCALROOT/$ORIGFILEPATH/CVS/Entries"
@@ -66,20 +90,7 @@ for fn in $FILELIST ; do
 	echo "Copying $ORIGFILENAME to $TARGETFILE"
 	cp $LOCALROOT/$ORIGFILE $TARGETFILE
 
-	# translate it into pascal and apply known conversions
-	if [ "x$H2PAS" != "x" ] ; then
-		echo "---------------- [$fn] -----------------" >>$TMP/h2pas.log
-		# translate
-		echo "Translating $TARGETFILE to $TARGETFILE.1.pas"
-		h2pas -d -c -i $TARGETFILE -o $TARGETFILE.1.pas 1>&2 >>$TMP/h2pas.log
-
-		# apply known replacements
-		echo "Applying additional conversions --> $TARGETFILE.2.pas"
-		sed -f "$LIBXML2_PAS/utils/afterconv.sed" $TARGETFILE.1.pas > $TARGETFILE.2.pas
-
-		# compare original translation and the one with replacements
-		diff $TARGETFILE.1.pas $TARGETFILE.2.pas >$TARGETFILE.pas.diff12
-	fi
+	toPascal $TARGETFILE
 
 	# find the CVSROOT for this file - it is stored with the CVS local copy
 	CVSROOT="`cat $LOCALROOT/$ORIGFILEPATH/CVS/Root`"
@@ -97,7 +108,54 @@ for fn in $FILELIST ; do
 	cd $LOCALROOT
 	$cmd >$THISPATH/$TARGETFILE.log
 	cd $THISPATH
-	CHGCOUNT=`expr $CHGCOUNT + 1`
+	CHGCOUNT=$((CHGCOUNT + 1))
+done
+
+# prepare list of files that do not use CVS SIGN and should not be reported as unconverted
+cat >$TMP/ignoredFiles <<EOF
+libxslt/libexslt/libexslt.h
+libxslt/libxslt/libxslt.h
+libxslt/libxslt/win32config.h
+libxslt/libxslt/xsltconfig.h
+EOF
+
+# find unconverted header files
+echo "Checking for other header files in used directories..."
+sort -u $TMP/allPaths1 >$TMP/allPaths
+rm $TMP/allPaths1
+for dir in `cat $TMP/allPaths`; do
+	echo "  $dir:"
+	for fn in $LOCALROOT/$dir/*.h; do
+		if [ ! -e "$fn" ]; then
+			echo "No header files."
+			break
+		fi
+		sfn=${fn:${#LOCALROOT}+1}
+		
+		# ignore used files
+		if grep -q '^'$sfn'$' $TMP/allFiles; then
+			continue
+		fi
+		
+		# ignore files explicitly listed for ignoring
+		if grep -q '^'$sfn'$' $TMP/ignoredFiles; then
+			continue
+		fi
+		
+		# report any other files
+		echo "CONVERTING $sfn"
+
+		ORIGFILENAME=`basename $sfn`
+		ORIGFILEPATH=`dirname $sfn`
+		TARGETFILE=$TMP/`basename $ORIGFILEPATH`/$ORIGFILENAME
+		mkdir -p `dirname $TARGETFILE`
+
+		echo "  copying $ORIGFILENAME to $TARGETFILE"
+		cp $LOCALROOT/$ORIGFILE $TARGETFILE
+		toPascal $TARGETFILE
+
+		CHGCOUNT=$((CHGCOUNT + 1))
+	done
 done
 
 if [ $CHGCOUNT -gt 0 ]; then
@@ -106,7 +164,7 @@ if [ $CHGCOUNT -gt 0 ]; then
 	zip -rD ../$RESULTFILE *
 	cd $THISPATH
 fi
-rm -rf $TMP
+#rm -rf $TMP
 
 echo "----------------------------"
 echo "Total: $CHGCOUNT files changed"
